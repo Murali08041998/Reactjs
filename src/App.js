9766e0b12cd882fa4c4f9beabbd8709e22131348
Muralithee <122270788+Muralithee@users.noreply.github.com>
2023-01-18 18:57:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  const intro="Hello reactjs";
+  return (
+    <div>
+      <h1>{intro}</h1>
+    </div>
+  );
+  // return React.createElement("div",null,React.createElement("h1",null,intro));
+}
+
+export default App;
